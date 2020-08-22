3a7bbed37d982adea5fe2eb450a0ca84d5e40471
김진우 <kjw2122@naver.com>
2020-08-22 23:05:03 +0900

    Initial commit

---
+++ src/test/java/com/foodgram/daroguzo/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.foodgram.daroguzo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
