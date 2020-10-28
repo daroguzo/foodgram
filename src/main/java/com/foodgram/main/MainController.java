e1b4129b4f2e99dbcc992427a486bd99fc751bd5
김진우 <kjw2122@naver.com>
2020-10-28 22:59:25 +0900

    directory 수정

--- src/main/java/com/foodgram/main/MainController.java
+++ src/main/java/com/foodgram/main/MainController.java
@@ -2,44 +2,21 @@ package com.foodgram.main;
 
 import com.foodgram.config.auth.LoginUser;
 import com.foodgram.config.dto.SessionUser;
-import com.foodgram.post.PostResponseDto;
-import com.foodgram.post.PostService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-
-import javax.servlet.http.HttpSession;
 
 @RequiredArgsConstructor
 @Controller
 public class MainController {
 
-    private final PostService postService;
-
     @GetMapping("/")
     public String index(Model model, @LoginUser SessionUser user) {
-        model.addAttribute("post", postService.findAllDesc());
-
         if(user != null){
             model.addAttribute("userName", user.getName());
         }
 
         return "index";
-    }
-
-    @GetMapping("/post/save")
-    public String postsSave() {
-        return "post-save";
-    }
-
-    @GetMapping("/post/update/{id}")
-    public String postsUpdate(@PathVariable Long id, Model model){
-
-        PostResponseDto dto = postService.findById(id);
-        model.addAttribute("post", dto);
-
-        return "post-update";
     }
 }
